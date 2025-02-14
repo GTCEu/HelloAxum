77f5e06f419c56854d7e9d2ee796a44e38d820f3
GTCEu <74142803+GTCEu@users.noreply.github.com>
2025-02-14 20:40:07 +0200

    thingimajig

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,53 @@
 #![allow(unused)]
 
-use std::net::SocketAddr;
-use axum::{extract::Query, response::{Html, IntoResponse}, routing::get, Router};
-use serde::{Deserialize};
+use std::{net::SocketAddr};
+use axum::{extract::{Path, Query}, response::{Html, IntoResponse}, routing::{get, get_service}, Router};
+use serde::{ser::Impossible, Deserialize};
+use tower_http::{services::ServeDir};
 
 #[tokio::main]
 async fn main() {
-    let router_hello: Router = Router::new().route(
-        "/hello",
-        get(handler_hello));
+    let router_all = Router::new()
+    .merge(routes_hello())
+    .fallback_service(route_static());
     
     let address= SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("!!LITSENING on {address}\n");
     axum_server::bind(address)
-        .serve(router_hello.into_make_service())
+        .serve(router_all.into_make_service())
         .await
         .unwrap();
-    
+
+    fn route_static() -> Router {
+        Router::new().fallback_service(get_service(ServeDir::new("./")))
+    }
+
+    fn routes_hello() -> Router {
+        Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/{name}", get(handler_hello2))
+    }
+
     #[derive(Debug, Deserialize)]
     struct HelloParams {
         name: Option<String>
     }
 
+    // e.g. `/hello?name=param`
     async fn handler_hello(params: Query<HelloParams>) -> impl IntoResponse {
         println!("!! {:<12} - handler_hello - {params:?}", "HANDLER");
 
         let name = params.name.as_deref().unwrap_or("Missing");
 
-        Html(format!(" 🐈 Hello {name} "))
+        Html(format!(" 🐈 Hello {name} ") )
     }
+
+    // e.g. `/hello2/param`
+    async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+        println!("!! {:<12} - handler_hello - {name:?}", "HANDLER");
+
+        Html(format!(" 🐈 Hello {name} ") )
+    };
+
 } //$ cargo install cargo-watch
 //$ cargo watch -q -c -w src/ -x run
--- tests/quick_dev.rs
+++ tests/quick_dev.rs
@@ -6,7 +6,9 @@ use anyhow::Result;
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080")?;
 
-    hc.do_get("/hello?name=World").await?.print().await?;
+    hc.do_get("/hello2/wawa").await?.print().await?;
     
+    hc.do_get("/src/main.rs").await?.print().await?;
+
     Ok(())
 } //$ cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"
