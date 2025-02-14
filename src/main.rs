c7df8302bc946106af3f1ea4d70ccca83ed279a3
GTCEu <74142803+GTCEu@users.noreply.github.com>
2025-02-14 19:05:34 +0200

    handler

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 #![allow(unused)]
 
 use std::net::SocketAddr;
-use axum::{response::Html, routing::get, Router};
+use axum::{response::{Html, IntoResponse}, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     let router_hello: Router = Router::new().route(
         "/hello",
-        get(|| async { Html("🐈") }),
-    );
-
+        get(handler_func));
+    
     let address= SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("!!LITSENING on {address}\n");
     axum_server::bind(address)
         .serve(router_hello.into_make_service())
         .await
         .unwrap();
+
+    async fn handler_func() -> impl IntoResponse {
+        println!("!! {:<12} - handler_hello", "HANDLER");
+
+        Html({" 🐈 "})
+    }
 } //$ cargo install cargo-watch
 //$ cargo watch -q -c -w src/ -x run
